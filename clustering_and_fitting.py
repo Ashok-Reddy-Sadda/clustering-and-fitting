2d9c043eb89271d55ef2db3987bf8bcf1bb28950
Ashok-Reddy-Sadda <ashokreddysadda@gmail.com>
2025-03-29 17:07:35 +0000

    Add files via upload

--- clustering_and_fitting.py
+++ clustering_and_fitting.py
@@ -177,14 +177,14 @@ def main():
     moments = statistical_analysis(df, col)
     writing(moments, col)
     clustering_results = perform_clustering(
-     df, 
-     'Sum of Males  Life Expectancy', 
+     df,
+     'Sum of Males  Life Expectancy',
      'Sum of Females  Life Expectancy'
     )
     plot_clustered_data(*clustering_results)
     fitting_results = perform_fitting(
-     df, 
-     'Sum of Males  Life Expectancy', 
+     df,
+     'Sum of Males  Life Expectancy',
      'Sum of Life Expectancy  (both sexes)'
     )
     plot_fitted_data(*fitting_results)
